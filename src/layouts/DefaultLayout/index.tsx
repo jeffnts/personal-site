71c427129f843b5da550f6d05d2b0928a38cb9ab
Jefferson Nascimento <jf.nts10@gmail.com>
2023-06-18 09:50:53 -0300

    fix: menu position

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function DefaultLayout({ children }: Props ){
     return (
         <div className="flex flex-col min-h-screen bg-zinc-100">
-            <header className="bg-gray-800 text-white p-4">
+            <header className="bg-gray-800 text-white p-4 fixed top-0 w-full z-50">
                 <Navbar />
             </header>
 
@@ -17,7 +17,7 @@ export default function DefaultLayout({ children }: Props ){
                 { children }
             </main>
 
-            <footer className="bg-gray-800 text-white p-4 fixed bottom-0 w-full">
+            <footer className="bg-gray-800 text-white p-4 w-full">
                 <p className="text-center">&copy; {`2023 - ${new Date().getFullYear()} | Jeff <dev />. Todos os direitos reservados`}.</p>
             </footer>
         </div>
